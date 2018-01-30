24d79480a724d0cd9725ae87f2ef60f67951efd8
Michael <michael@gocatch.com>
2018-01-30 20:16:11 +1100

    added more tests, removed redundant files, added docstrings, refactors some functions

--- src/bowling_challenge/core.clj
+++ src/bowling_challenge/core.clj
@@ -8,94 +8,137 @@
   (println "Hello, World!"))
 
 
-(defn new-score-sheet []
+(defn new-empty-frame
+  "Takes in the number of the frame and return an empty frame with no scores."
+  [n]
+  {:frame        n
+   :ball-score  []
+   :frame-score 0})
+
+
+(defn new-score-sheet
+  "Create a new blank score sheet in the form of a list of maps.
+  Each map represents a frame and consists of a frame number, result of two bowls and the score of the frame"
+  []
   (loop [n   0
          acc []]
     (if (= n 11)
       acc
       (recur (inc n)
              (conj acc
-                   (let [rand1 (rand-int 11)
-                         rand2 (rand-int (- 11 rand1))]
-                     {:frame      n
-                      :ball-score [10 0]
-                      :frame-score 0}))))))
+                   (new-empty-frame n))))))
 
 
-(defn strike-or-spare [pins_down]
-  (cond (= (first pins_down) 10)
+(defn strike-or-spare
+  "Given the number of 2 bowls in a frame as `pins-down`, return the result of frame.
+  The `pins-down` is a list given in the form [int int]."
+  [pins-down]
+  (cond (= (first pins-down) 10)
         :strike
-        (= (+ (first pins_down) (second pins_down)) 10)
+        (= (+ (first pins-down) (second pins-down)) 10)
         :spare
         :else
         :open))
 
 
-(defn no-of-frames-needed [frame]
-  (let [current_frame_state (strike-or-spare (:ball-score frame))
+(defn no-of-frames-needed
+  "Takes in a single `frame` and figures out how many frames are needed to figure out the score."
+  [frame]
+  (let [current-frame-state (strike-or-spare (:ball-score frame))
         frame-number (:frame frame)]
-    (cond (= current_frame_state :open)
-          1
-          (or (= current_frame_state :spare)
-              (= frame-number 9))
-          2
-          (= current_frame_state :strike)
-          3)))
+    (cond (= current-frame-state :open) 1
+          (or (= current-frame-state :spare)
+              (= frame-number 9)) 2
+          (= current-frame-state :strike) 3)))
 
 
-(defn pick-frames-to-use [frames_needed]
-  (if (= 3 (count frames_needed))
-    (if (not= :strike (strike-or-spare (:ball-score (second frames_needed))))
-      (take 2 frames_needed)
-      frames_needed)
-    frames_needed))
+(defn pick-frames-to-use
+  "This is used for determining which of the 3 frames are used for a strike.
+  Only 3 frames are needed if there are 2 strikes in a row."
+  [frames-needed]
+  (if (= 3 (count frames-needed))
+    (if (not= :strike (strike-or-spare (:ball-score (second frames-needed))))
+      (take 2 frames-needed)
+      frames-needed)
+    frames-needed))
 
 
-(defn score-frame [frames_needed]
-  (let [list_of_scores (->>  frames_needed
+(defn score-frame
+  "Given the frames needed to determine a sc ore for the frame, calculate the score of a frame.
+  A list of maps is used as `frames-needed`.
+  Also take into account the bonus bowls from strike and spare in the 10th frame"
+  [frames-needed]
+  (let [list-of-scores (->>  frames-needed
                              (map :ball-score)
                              (reduce concat))]
-    (if (or (= :spare (strike-or-spare (:ball-score (first frames_needed))))
-            (and (= 9 (:frame (second frames_needed)))
-                 (= :strike (strike-or-spare (:ball-score (second frames_needed)))))) ;;NOT HAPPY WITH THIS!
-      (->> list_of_scores
+    (if (or (= :spare (strike-or-spare (:ball-score (first frames-needed)))) ;; HACK? : Used to calculate spare or 10th frame.
+            (and (= 9 (:frame (second frames-needed)))
+                 (= :strike (strike-or-spare (:ball-score (second frames-needed))))))
+      (->> list-of-scores
            drop-last
            (reduce +))
-      (reduce + list_of_scores))))
+      (reduce + list-of-scores))))
 
 
-(defn add-frame-score! [score_card frame_no]
-  (let [coll       (nthrest @score_card frame_no)
+(defn assoc-frame-score!
+  "Assoc the score of a frame in a list of maps.
+  Takes in args `score-card` and `frame-no` in the form [atom int]"
+  [score-card frame-no]
+  (let [coll       (nthrest @score-card frame-no)
         value      (-> (first coll)
                        no-of-frames-needed
                        (take coll)
                        pick-frames-to-use
                        score-frame)]
-    (swap! score_card update-in [frame_no] assoc :frame-score value)))
+    (swap! score-card update-in [frame-no] assoc :frame-score value)))
 
 
-(defn traverse-board [score_card]
-  (loop [n @score_card]
+(defn traverse-board
+  "Traverse a list of frames and assoc the resulting score of the frame."
+  [score-card]
+  (loop [n @score-card]
     (if (empty? (:ball-score (second n)))
-      score_card
-      (recur (do (add-frame-score! score_card (:frame (first n)))
-                 (rest n))))))
-
-(defn total [score_card]
-  (reduce + (map :frame-score @score_card)))
+      score-card
+      (recur (do (assoc-frame-score! score-card (:frame (first n)))
+                 (rest n)))))) ;; Can also be done using doseq on the list.
 
 
-(defn finish? [score_card]
-  (let [bonus_bowls   (count (:ball-score (last @score_card)))
-        final_frame_status (strike-or-spare (:ball-score(nth @score_card 9)))]
-    (if (or (= :open final_frame_status)
-            (and (or (= :spare final_frame_status)
-                     (= :strike final_frame_status))
-                 (= 2 bonus_bowls)))
-      (total score_card)
-      false)))
+(defn total-score
+  "Returns the score of a whole game."
+  [score-card]
+  (->> @score-card
+       drop-last  ;;drop-last to remove final bonus frame for strike and spare
+       (map :frame-score)
+       (reduce +)))
 
 
+(defn count-frames-used
+  "Count number of frames played.
+  Given a list of maps, determine how many frames have been played by looking at the score or bowls.
+  Blank scores indicate a record has not been recorded while 0 indicates a gutter ball,"
+  [score-card]
+  (when ((complement empty?) (:ball-score (first @score-card)))
+    (loop [n     @score-card
+           count 0]
+      (if (empty? (:ball-score (first n)))
+        count
+        (recur (rest n) (inc count))))))
 
-:end_bowling_core
-0
+
+(defn finish? ;; Maybe not use '?' in function name as it is not really a predicate.
+  "Check if a game is finished.
+  Return score of the game if it is finished or return false if game is incomplete."
+  [score-card]
+  (if (>= (count-frames-used score-card) 10)
+    (let [bonus-bowls   (count (:ball-score (last @score-card)))
+          final-frame-status (strike-or-spare (:ball-score(nth @score-card 9)))]
+      (if (or (= :open final-frame-status)
+              (and (or (= :spare final-frame-status)
+                       (= :strike final-frame-status))
+                   (= 2 bonus-bowls)))
+        (total-score score-card)
+        false))
+    false))
+
+
+:end-bowling-core
--- test/bowling_challenge/core_test.clj
+++ test/bowling_challenge/core_test.clj
@@ -6,6 +6,58 @@
   (testing "FIXME, I fail."
     (is (= 1 1))))
 
+(def strike-bowl [10 0])
+(def spare-bowl [9 1])
+(def open-bowl [0 9])
+
+(deftest strike-or-spare-test
+  (testing "Strike bowl"
+    (is (= :strike
+           (strike-or-spare strike-bowl))))
+  (testing "Spare bowl"
+    (is (= :spare
+           (strike-or-spare spare-bowl))))
+  (testing "Open bowl"
+    (is (= :open
+           (strike-or-spare open-bowl)))))
+
+(def strike-frame {:frame 0, :ball-score [10 0], :frame-score 0})
+(def spare-frame {:frame 0, :ball-score [1 9], :frame-score 0})
+(def open-frame {:frame 0, :ball-score [1 0], :frame-score 0})
+
+(deftest no-of-frames-needed-test
+  (testing "Strike frame with no-of-frames-needed-test."
+    (is (= 3
+           (no-of-frames-needed strike-frame))))
+  (testing "Spare frame with no-of-frames-needed-test."
+    (is (= 2
+           (no-of-frames-needed spare-frame))))
+  (testing "Open frame with no-of-frames-needed-test."
+    (is (= 1
+           (no-of-frames-needed open-frame)))))
+
+(def single-strike [{:frame 0, :ball-score [10 0], :frame-score 0}
+                    {:frame 1, :ball-score [1 1], :frame-score 0}
+                    {:frame 2, :ball-score [1 1], :frame-score 0}])
+
+(def double-strike [{:frame 0, :ball-score [10 0], :frame-score 0}
+                    {:frame 1, :ball-score [10 0], :frame-score 0}
+                    {:frame 2, :ball-score [1 1], :frame-score 0}])
+
+(def spare [{:frame 0, :ball-score [9 1], :frame-score 0}
+            {:frame 1, :ball-score [1 1], :frame-score 0}])
+
+(deftest pick-frames-to-use-test
+  (testing "Pick frames of single strike."
+    (is (= (take 2 single-strike)
+           (pick-frames-to-use single-strike))))
+  (testing "Pick frames of double strike."
+    (is (= double-strike
+           (pick-frames-to-use double-strike))))
+  (testing "Pick frames of spare"
+    (is (= spare
+           (pick-frames-to-use spare)))))
+
 (def all-strike (atom [{:frame 0, :ball-score [10 0], :frame-score 0}
                        {:frame 1, :ball-score [10 0], :frame-score 0}
                        {:frame 2, :ball-score [10 0], :frame-score 0}
@@ -54,19 +106,19 @@
                           {:frame 9, :ball-score [9 1], :frame-score 0}
                           {:frame 10, :ball-score [9 0], :frame-score 0}]))
 
-(def all-spare-strike-alternate (atom [{:frame 0, :ball-score [1 9], :frame-score 0}
-                                       {:frame 1, :ball-score [10 0], :frame-score 0}
-                                       {:frame 2, :ball-score [1 9], :frame-score 0}
-                                       {:frame 3, :ball-score [10 0], :frame-score 0}
-                                       {:frame 4, :ball-score [1 9], :frame-score 0}
-                                       {:frame 5, :ball-score [10 0], :frame-score 0}
-                                       {:frame 6, :ball-score [1 9], :frame-score 0}
-                                       {:frame 7, :ball-score [1 9], :frame-score 0}
-                                       {:frame 8, :ball-score [10 0], :frame-score 0}
-                                       {:frame 9, :ball-score [1 9], :frame-score 0}
-                                       {:frame 10, :ball-score [10], :frame-score 0}]))
+(def all-spare-and-strike (atom [{:frame 0, :ball-score [1 9], :frame-score 0}
+                             {:frame 1, :ball-score [10 0], :frame-score 0}
+                             {:frame 2, :ball-score [1 9], :frame-score 0}
+                             {:frame 3, :ball-score [10 0], :frame-score 0}
+                             {:frame 4, :ball-score [1 9], :frame-score 0}
+                             {:frame 5, :ball-score [10 0], :frame-score 0}
+                             {:frame 6, :ball-score [1 9], :frame-score 0}
+                             {:frame 7, :ball-score [1 9], :frame-score 0}
+                             {:frame 8, :ball-score [10 0], :frame-score 0}
+                             {:frame 9, :ball-score [1 9], :frame-score 0}
+                             {:frame 10, :ball-score [10 0], :frame-score 0}]))
 
-(def incomplete (atom [{:frame 0, :ball-score [1 9], :frame-score 0}
+(def incomplete-game (atom [{:frame 0, :ball-score [1 9], :frame-score 0}
                        {:frame 1, :ball-score [10 0], :frame-score 0}
                        {:frame 2, :ball-score [1 9], :frame-score 0}
                        {:frame 3, :ball-score [10 0], :frame-score 0}
@@ -74,11 +126,11 @@
                        {:frame 5, :ball-score [10 0], :frame-score 0}
                        {:frame 6, :ball-score [1 9], :frame-score 0}
                        {:frame 7, :ball-score [1 9], :frame-score 0}
-                       {:frame 8, :ball-score [], :frame-score 0}
-                       {:frame 9, :ball-score [], :frame-score 0}
+                       {:frame 8, :ball-score [1 9], :frame-score 0}
+                       {:frame 9, :ball-score [10 0], :frame-score 0}
                        {:frame 10, :ball-score [], :frame-score 0}]))
 
-(deftest finish!
+(deftest finish?-test
   (testing "All strike game"
     (is (= 300
            (finish? (traverse-board all-strike)))))
@@ -91,6 +143,9 @@
   (testing "All spare 9-1 game"
     (is (= 190
            (finish? (traverse-board all-9-1-spare)))))
-  (testing "Alternating spare strike"
+(testing "All strike and spare"
+    (is (= 191
+           (finish? (traverse-board all-spare-and-strike)))))
+  (testing "Incomplete game"
     (is (= false
-           (finish? (traverse-board all-spare-strike-alternate))))))
+           (finish? (traverse-board incomplete-game))))))
